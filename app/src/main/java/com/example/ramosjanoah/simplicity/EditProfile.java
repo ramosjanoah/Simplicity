fd57706301c3076f83f518007decbcfaa1b5ee86
alvinjunianto <alvinjunianto@gmail.com>
2017-02-24 19:12:38 +0700

    homepage background added, button save on edit profile action set to back to homepage

--- app/src/main/java/com/example/ramosjanoah/simplicity/EditProfile.java
+++ app/src/main/java/com/example/ramosjanoah/simplicity/EditProfile.java
@@ -1,9 +1,11 @@
 package com.example.ramosjanoah.simplicity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -15,5 +17,10 @@ public class EditProfile extends AppCompatActivity {
         if(configInfo.orientation == Configuration.ORIENTATION_LANDSCAPE){
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
         }
+    }
+
+    public void saveEdit(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
