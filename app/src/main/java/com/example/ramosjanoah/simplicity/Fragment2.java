b26741d3f9488928ea1d9bdb160922f1871c7f56
Azudeus <blitzthunder68@yahoo.com>
2017-02-24 17:44:43 +0700

    Fixed minor bug fragment 2

--- app/src/main/java/com/example/ramosjanoah/simplicity/Fragment2.java
+++ app/src/main/java/com/example/ramosjanoah/simplicity/Fragment2.java
@@ -75,7 +75,7 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_fragment1, container, false);
+        View view=inflater.inflate(R.layout.fragment_fragment2, container, false);
         HealthTextView = (TextView) view.findViewById(R.id.HealthTextView);
         MuscleTextView = (TextView) view.findViewById(R.id.MuscleTextView);
         return view;
