fcd09018f5413696909926ed56c9cd3702ebf166
alvinjunianto <alvinjunianto@gmail.com>
2017-02-24 18:02:57 +0700

    ignore rotate screen on edit profile

--- app/src/main/java/com/example/ramosjanoah/simplicity/EditProfile.java
+++ app/src/main/java/com/example/ramosjanoah/simplicity/EditProfile.java
@@ -1,5 +1,7 @@
 package com.example.ramosjanoah.simplicity;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,9 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        Configuration configInfo = getResources().getConfiguration();
+        if(configInfo.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+        }
     }
 }
